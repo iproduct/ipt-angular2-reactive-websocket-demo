70e376d1e6146d6854d493a0bf9c264d4c5b7146
iproduct <office@iproduct.org>
2015-12-11 17:24:21 +0200

    Caching disabled for easier testing.

--- IPT_Reactor_Demo_JUG_Wishes_1_0/src/main/java/reactor/ReactorWishesWS.java
+++ IPT_Reactor_Demo_JUG_Wishes_1_0/src/main/java/reactor/ReactorWishesWS.java
@@ -6,7 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.ConcurrentHashMap;
@@ -33,7 +32,7 @@ public class ReactorWishesWS {
 	private Broadcaster<String> channelBroadcaster;
 //	private EventBus serverReactor;
 	
-	private final Map<String,String> cache = new HashMap<>();
+//	private final Map<String,String> cache = new HashMap<>();
 	private final Map<String,Integer> wishes = new ConcurrentHashMap<>();
 	Wish[] sampleWishes = {
 		new Wish("RxJava", 15 ),
